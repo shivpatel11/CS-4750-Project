e8dacb09ab23eb971ad9fa08fa72186b90ba87d4
Shiv Patel <toshiv31@gmail.com>
2021-09-14 10:25:44 -0700

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,313 @@
+import 'package:coffeeorder/details_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'details_page.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+
+        body: ListView(
+          padding: EdgeInsets.only(left: 15.0),
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 50.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                    'Welcome To My Shop',
+                    style: TextStyle(
+                        fontFamily: 'varela',
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF473D3A)
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.only(right: 15.0),
+                    child: Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/model.jpg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    )
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 45.0),
+              child: Container(
+                child: Text(
+                  'Let\'s pick the best coffee for your next drink!',
+                  style: TextStyle(
+                      fontFamily: 'nunito',
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w300,
+                      color: Color(0xFFB0AAA7)),
+                ),
+              ),
             ),
+            SizedBox(height: 25.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Coffee of the week',
+                  style: TextStyle(
+                      fontFamily: 'varela',
+                      fontSize: 17.0,
+                      color: Color(0xFF473D3A)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right:15.0),
+                  child: Text('View Choices',
+                    style: TextStyle(
+                        fontFamily: 'varela',
+                        fontSize: 15.0,
+                        color: Color(0xFFCEC7C4)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Container(
+                height: 410.0,
+                child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      _coffeeListCard('assets/starbucks.png',
+                          'Pumpkin Spice',
+                          'Starbucks',
+                          'Whip up a Caramel Pumpkin Spice Latte and enjoy an extra layer of deliciousness in this caramel-flavored twist on a fall favorite.',
+                          '\$5.25',
+                          false
+                      ),
+                      _coffeeListCard('assets/starbucks.png',
+                          'Chai Latte',
+                          'Starbucks',
+                          'Black tea infused with cinnamon, clove and other warming spices',
+                          '\$3.65',
+                          false
+                      )
+                    ]
+                )
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Explore nearby',
+                  style: TextStyle(
+                      fontFamily: 'varela',
+                      fontSize: 17.0,
+                      color: Color(0xFF473D3A)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 15.0),
+                  child: Text(
+                    'See All',
+                    style: TextStyle(
+                        fontFamily: 'varela',
+                        fontSize: 15.0,
+                        color: Color(0xFFCEC7C4)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Container(
+                height: 125.0,
+                child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      _buildImage('assets/coffee.jpg'),
+                      _buildImage('assets/coffee2.jpg'),
+                      _buildImage('assets/coffee3.jpg')
+                    ]
+                )
+            ),
+            SizedBox(height: 20.0)
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
+
+    );
+  }
+
+  _buildImage(String imgPath) {
+    return Padding(
+        padding: EdgeInsets.only(right: 15.0),
+        child: Container(
+            height: 100.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                image: DecorationImage(image: AssetImage(imgPath), fit: BoxFit.cover)
+            )
+        )
+    );
+  }
+
+  _coffeeListCard(String imgPath, String coffeeName, String shopName, String description, String price, bool isFavorite) {
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Container(
+            height: 300.0,
+            width: 225.0,
+            child: Column(
+              children: <Widget>[
+                Stack(
+                    children: [
+                      Container(
+                          height: 335.0
+                      ),
+                      Positioned(
+                          top: 75.0,
+                          child: Container(
+                              padding: EdgeInsets.only(left: 10.0, right: 20.0),
+                              height: 260.0,
+                              width: 225.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  color: Color(0xFFDAB68C)
+                              ),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      height: 60.0,
+                                    ),
+                                    Text(
+                                      shopName + '\'s',
+                                      style: TextStyle(
+                                          fontFamily: 'nunito',
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Text(
+                                      coffeeName,
+                                      style: TextStyle(
+                                          fontFamily: 'varela',
+                                          fontSize: 32.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Text(
+                                      description,
+                                      style: TextStyle(
+                                          fontFamily: 'nunito',
+                                          fontSize: 14.0,
+                                          // fontWeight: FontWeight.w300,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          price,
+                                          style: TextStyle(
+                                              fontFamily: 'varela',
+                                              fontSize: 25.0,
+                                              fontWeight: FontWeight.bold,
+                                              color: Color(0xFF3A4742)),
+                                        ),
+                                        Container(
+                                            height: 40.0,
+                                            width: 40.0,
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(20.0),
+                                                color: Colors.white
+                                            ),
+                                            child: Center(
+                                                child: Icon(Icons.favorite,
+                                                    color: isFavorite ? Colors.red: Colors.grey,
+                                                    size: 15.0
+                                                )
+                                            )
+                                        )
+                                      ],
+                                    )
+                                  ]
+                              )
+                          )
+                      ),
+                      Positioned(
+                          left: 60.0,
+                          top: 25.0,
+                          child: Container(
+                              height: 100.0,
+                              width: 100.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(imgPath),
+                                      fit: BoxFit.contain
+                                  )
+                              )
+                          )
+                      )
+                    ]
+                ),
+                SizedBox(height: 20.0),
+                InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DetailsPage()
+                      ));
+                    },
+                    child: Container(
+                        height: 50.0,
+                        width: 225.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25.0),
+                            color: Color(0xFF473D3A)
+                        ),
+                        child: Center(
+                            child: Text(
+                                'Order Now',
+                                style: TextStyle(
+                                    fontFamily: 'nunito',
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                )
+                            )
+                        )
+                    )
+                )
+              ],
+            )
+        )
     );
   }
 }
